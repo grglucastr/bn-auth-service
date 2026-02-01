f841eae544fc9c2660bcd7708900b8ae204d8b9b
George Nunes <georgebnunes@gmail.com>
2026-02-01 19:05:15 -0300

    feat: set timezone

--- src/main/java/com/github/grglucastr/bnauthservice/Application.java
+++ src/main/java/com/github/grglucastr/bnauthservice/Application.java
@@ -1,11 +1,14 @@
 package com.github.grglucastr.bnauthservice;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import java.util.TimeZone;
 
 @SpringBootApplication(exclude = {
         DataSourceAutoConfiguration.class,
@@ -14,6 +17,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 })
 @EnableWebSecurity
 public class Application {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 
     void main(String[] args) {
         SpringApplication.run(Application.class, args);
