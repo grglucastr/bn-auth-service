1bcbd4d44ec62da9939a5576127689acf2b25af3
George Nunes <georgebnunes@gmail.com>
2026-02-12 14:22:35 -0300

    feat: service to persist user

---
+++ src/main/java/com/github/grglucastr/bnauthservice/service/UserService.java
@@ -0,0 +1,44 @@
+package com.github.grglucastr.bnauthservice.service;
+
+import com.github.grglucastr.bnauthservice.entity.User;
+import com.github.grglucastr.bnauthservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public User registerUser(String username, String email, String password) {
+
+        if (userRepository.existsByUsername(username)) {
+            throw new IllegalArgumentException("Username already exists");
+        }
+
+        if (userRepository.existsByEmail(email)) {
+            throw new IllegalArgumentException("Email already exists");
+        }
+
+        User user = User.builder()
+                .username(username)
+                .email(email)
+                .password(passwordEncoder.encode(password))
+                .roles(Set.of("USER"))
+                .enabled(true)
+                .build();
+
+        User userSaved = userRepository.save(user);
+        log.info("New user registered: {}", username);
+        return userSaved;
+    }
+}
