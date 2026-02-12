248163437f88f367ad921819d8709b7f9e26b9bf
George Nunes <georgebnunes@gmail.com>
2026-02-12 16:30:19 -0300

    feat: add refresh token entity

---
+++ src/main/java/com/github/grglucastr/bnauthservice/entity/RefreshToken.java
@@ -0,0 +1,48 @@
+package com.github.grglucastr.bnauthservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "expires_at", nullable = false)
+    private LocalDateTime expiresAt;
+
+    @Column(name = "revoked")
+    private Boolean revoked = Boolean.FALSE;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    public boolean  isExpired() {
+        return LocalDateTime.now().isAfter(expiresAt);
+    }
+
+}
