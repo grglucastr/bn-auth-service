a644b0e3c002f19e218dcf5adfd28d02cd8b8803
George Nunes <georgebnunes@gmail.com>
2026-02-12 14:33:23 -0300

    remove: unused imports

--- src/main/java/com/github/grglucastr/bnauthservice/config/SecurityConfig.java
+++ src/main/java/com/github/grglucastr/bnauthservice/config/SecurityConfig.java
@@ -9,16 +9,10 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
