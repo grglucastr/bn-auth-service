e085dee199c71de24627bb1093c80cb36c7a614e
George Nunes <georgebnunes@gmail.com>
2026-02-16 21:51:57 -0300

    feat: email verification template

--- src/main/java/com/github/grglucastr/bnauthservice/service/EmailService.java
+++ src/main/java/com/github/grglucastr/bnauthservice/service/EmailService.java
@@ -25,4 +25,20 @@ public class EmailService {
 
         log.info("=".repeat(80));
     }
+
+    public void sendVerificationEmail(String email, String verificationToken) {
+        String verificationLink = "http://localhost:8080/api/v1/auth/verify-email?token=" + verificationToken;
+
+        log.info("=".repeat(80));
+        log.info("📧 PASSWORD RESET MAIL (Simulated)");
+        log.info("To: {}", email);
+        log.info("Subject: Verify Your Email Address");
+        log.info("");
+        log.info("Welcome! Please verify your email address by clicking the link below: ");
+        log.info("{}", verificationLink);
+        log.info("");
+        log.info("This link will expire in 24 hour.");
+        log.info("If you didn't request this, please ignore this email.");
+        log.info("=".repeat(80));
+    }
 }
